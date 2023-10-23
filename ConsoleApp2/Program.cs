499ba734506285a16f54bc630afdded5e9f815fa
hyunbaeKim <hyunbae9242@gmail.com>
2023-10-23 20:09:14 +0900

    init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp2 // Note: actual namespace depends on the project name.
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
